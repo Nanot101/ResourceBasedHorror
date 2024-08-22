4baf8a98bbb7010400741578d52277206aa1af97
DriplessAtoms <78892280+DriplessAtoms@users.noreply.github.com>
2024-08-22 02:51:43 -0700

    Mouse Aim Implemented

---
+++ RBH/Assets/Scripts/Player/PlayerMouseAim.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMouseAim : MonoBehaviour
+{
+    private float mouseX;
+    private float mouseY;
+
+    void Update()
+    {
+        mouseX = Input.mousePosition.x - (Screen.width / 2);
+        mouseY = Input.mousePosition.y - (Screen.height / 2);
+
+        Debug.Log(Mathf.Atan(mouseY/mouseX) * Mathf.Rad2Deg);
+        transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y, Mathf.Atan2(mouseY, mouseX) * Mathf.Rad2Deg - 90);
+    }
+}

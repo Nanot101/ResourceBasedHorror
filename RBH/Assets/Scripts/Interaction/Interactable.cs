4530e4aab4a74f6a914595fe46bc0875eaf4652f
RafalPujer <rafal.pujer00@gmail.com>
2024-09-03 23:15:13 +0200

    Moved the interaction call from the interactable class to the player interaction caller class

--- RBH/Assets/Scripts/Interaction/Interactable.cs
+++ RBH/Assets/Scripts/Interaction/Interactable.cs
@@ -1,6 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using UnityEngine;
 
 public class Interactable : MonoBehaviour
@@ -8,10 +5,10 @@ public class Interactable : MonoBehaviour
     [SerializeField]
     private InteractableUI UI;
 
-    [SerializeField]
-    private InteractionBase interaction;
+    [field: SerializeField]
+    public InteractionBase Interaction { get; private set; }
 
-    private IInteractionCaller currentCaller = null;
+    public Vector2 Position => gameObject.transform.position;
 
     // Start is called before the first frame update
     void Start()
@@ -22,76 +19,14 @@ public class Interactable : MonoBehaviour
             Destroy(this);
         }
 
-        if (interaction == null)
+        if (Interaction == null)
         {
             Debug.LogError("Interaction is required for interactable");
             Destroy(this);
         }
     }
 
-    private void Update()
-    {
-        if (currentCaller == null)
-        {
-            UI.Hide();
-            return;
-        }
+    public void ShowUI() => UI.Show();
 
-        if (!interaction.CanInteract(currentCaller))
-        {
-            UI.Hide();
-            return;
-        }
-
-        UI.Show();
-
-        if (!Input.GetKeyDown(KeyCode.E))
-        {
-            return;
-        }
-
-        interaction.Interact(currentCaller);
-    }
-
-    private void OnTriggerEnter2D(Collider2D collider)
-    {
-        if (currentCaller != null)
-        {
-            //Debug.Log("More than one caller is not supported.");
-            return;
-        }
-
-        if (!collider.TryGetComponent<IInteractionCaller>(out var caller))
-        {
-            //Debug.Log("Collider doesn't have interaction caller");
-            return;
-        }
-
-        currentCaller = caller;
-        //Debug.Log("Current caller set");
-    }
-
-    private void OnTriggerExit2D(Collider2D collider)
-    {
-        if (currentCaller == null)
-        {
-            //Debug.Log("Current caller is null");
-            return;
-        }
-
-        if (!collider.TryGetComponent<IInteractionCaller>(out var caller))
-        {
-            //Debug.Log("Collider doesn't have interaction caller");
-            return;
-        }
-
-        if (caller != currentCaller)
-        {
-            //Debug.Log("Exited collider isn't current caller");
-            return;
-        }
-
-        currentCaller = null;
-        //Debug.Log("Current caller removed");
-    }
+    public void HideUI() => UI.Hide();
 }
--- RBH/Assets/Scripts/Player/Interaction/PlayerInteractionCaller.cs
+++ RBH/Assets/Scripts/Player/Interaction/PlayerInteractionCaller.cs
@@ -1,8 +1,100 @@
-using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class PlayerInteractionCaller : MonoBehaviour, IInteractionCaller
 {
+    private List<Interactable> interactables = new();
+
+    private Interactable selectedInteracteable = null;
+
     public GameObject GameObject => gameObject;
+
+    private void Update()
+    {
+        SetInteractable(GetClosestPossibleInteractable());
+
+        TryInteract();
+    }
+
+    private Interactable GetClosestPossibleInteractable()
+        => interactables
+        .Where(x => x.Interaction.CanInteract(this))
+        .OrderBy(x => Vector2.Distance(gameObject.transform.position, x.Position))
+        .FirstOrDefault();
+
+    private void SetInteractable(Interactable interactable)
+    {
+        if (selectedInteracteable == interactable)
+        {
+            return;
+        }
+
+        if (selectedInteracteable != null)
+        {
+            selectedInteracteable.HideUI();
+        }
+
+        selectedInteracteable = interactable;
+
+        if (selectedInteracteable != null)
+        {
+            selectedInteracteable.ShowUI();
+        }
+    }
+
+    private void TryInteract()
+    {
+        if (selectedInteracteable == null)
+        {
+            return;
+        }
+
+        if (!Input.GetKeyDown(KeyCode.E))
+        {
+            return;
+        }
+
+        selectedInteracteable.Interaction.Interact(this);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (!collider.TryGetComponent<Interactable>(out var interactable))
+        {
+            //Debug.Log("Player interaction caller didn't enter interactable");
+            return;
+        }
+
+        if (!interactables.Contains(interactable))
+        {
+            interactables.Add(interactable);
+
+            //Debug.Log("interactable added");
+
+            //return;
+        }
+
+        //Debug.Log("interactable already added");
+    }
+
+    private void OnTriggerExit2D(Collider2D collider)
+    {
+        if (!collider.TryGetComponent<Interactable>(out var interactable))
+        {
+            //Debug.Log("Player interaction caller didn't exit interactable");
+            return;
+        }
+
+        if (interactables.Contains(interactable))
+        {
+            interactables.Remove(interactable);
+
+            //Debug.Log("interactable removed");
+
+            //return;
+        }
+
+        //Debug.Log("interactable wasn't in the list");
+    }
 }

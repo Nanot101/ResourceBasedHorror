02b6466c243daa688c9e21b9784164e2ebc10f16
Rafal Pujer <rafal.pujer00@gmail.com>
2025-01-10 17:01:26 +0100

    Added lifetime to enemy drop item

--- RBH/Assets/Scripts/Enemy/EnemyDropSystem/EnemyDropItem.cs
+++ RBH/Assets/Scripts/Enemy/EnemyDropSystem/EnemyDropItem.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using System.Collections.Generic;
 using InventorySystem;
 using UnityEngine;
@@ -9,15 +10,38 @@ public class EnemyDropItem : MonoBehaviour
 
     [SerializeField] private new Rigidbody2D rigidbody;
 
-    public ItemData ItemData { get; private set; }
+    [SerializeField] private float lifetimeInSeconds = 60.0f;
 
-    public List<ItemStack> ItemStacks { get; private set; }
+    private ItemData itemData;
+    private List<ItemStack> itemStacks;
+
+    public ItemData ItemData
+    {
+        get
+        {
+            RefreshLifetime();
+            return itemData;
+        }
+        private set { itemData = value; }
+    }
+
+    public List<ItemStack> ItemStacks
+    {
+        get
+        {
+            RefreshLifetime();
+            return itemStacks;
+        }
+        private set { itemStacks = value; }
+    }
 
     // Start is called before the first frame update
     void Start()
     {
         Debug.Assert(spriteRenderer != null, $"{nameof(spriteRenderer)} is required for {nameof(EnemyDropItem)}", this);
         Debug.Assert(rigidbody != null, $"{nameof(rigidbody)} is required for {nameof(EnemyDropItem)}", this);
+
+        StartCoroutine(DestroyCoroutine());
     }
 
     public void Init(ItemData itemData, List<ItemStack> itemStacks)
@@ -50,5 +74,19 @@ public class EnemyDropItem : MonoBehaviour
         gameObject.name = currentGameObjectName + " (" + ItemData.name + ")";
 
         spriteRenderer.sprite = ItemData.ItemSprite;
+    }
+
+    private IEnumerator DestroyCoroutine()
+    {
+        yield return new WaitForSeconds(lifetimeInSeconds);
+
+        Destroy(gameObject);
+    }
+
+    private void RefreshLifetime()
+    {
+        StopAllCoroutines();
+
+        StartCoroutine(DestroyCoroutine());
     }
 }

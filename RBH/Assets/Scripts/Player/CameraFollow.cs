d02ed2898c6b72fe1a386d35f3fba3ef7aa4c270
Rafal Pujer <rafal.pujer00@gmail.com>
2025-01-29 18:30:51 +0100

    Added smooth damp to camera follow

--- RBH/Assets/Scripts/Player/CameraFollow.cs
+++ RBH/Assets/Scripts/Player/CameraFollow.cs
@@ -1,13 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class CameraFollow : MonoBehaviour
 {
-    public Transform player;
+    [SerializeField] private Transform player;
 
-    void Update()
+    [Tooltip("Approximately the time it will take to reach the player. A smaller value will reach the player faster.")]
+    [SerializeField] private float smoothTime = 0.15f;
+
+    private Vector3 velocity = Vector3.zero;
+
+    private void Update()
     {
-        transform.position = new Vector3(player.transform.position.x, player.transform.position.y, transform.position.z);
+        var playerPos = player.position;
+
+        var targetCameraPos = new Vector3(playerPos.x, playerPos.y, transform.position.z);
+
+        transform.position =
+            Vector3.SmoothDamp(transform.position, targetCameraPos, ref velocity, smoothTime, Mathf.Infinity,
+                Time.unscaledDeltaTime);
     }
 }

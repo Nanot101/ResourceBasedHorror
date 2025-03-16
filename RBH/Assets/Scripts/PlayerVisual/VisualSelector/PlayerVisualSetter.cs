8ff8c2097d8775d21f03bd64fa47b418f13d1b68
Nanot101 <luangiang001@gmail.com>
2025-03-15 23:20:45 -0700

    visual updates
    renamed scenes, updated female visuals, revised visual scripts and scriptable objects

--- RBH/Assets/Scripts/PlayerVisual/VisualSelector/PlayerVisualSetter.cs
+++ RBH/Assets/Scripts/PlayerVisual/VisualSelector/PlayerVisualSetter.cs
@@ -1,21 +1,51 @@
+// using UnityEngine;
+
+// [RequireComponent(typeof(SpriteRenderer))]
+// public class PlayerVisualSetter : MonoBehaviour
+// {
+//     [SerializeField]
+//     private SpriteRenderer spriteRenderer;
+
+//     private void Start()
+//     {
+//         Debug.Assert(spriteRenderer != null, $"Sprite Renderer is required for {nameof(PlayerVisualSetter)}");
+//     }
+
+//     public void SetVisual(PlayerVisual visual)
+//     {
+//         Debug.Assert(visual != null);
+
+//         spriteRenderer.sprite = visual.Sprite;
+//         spriteRenderer.color = visual.Color;
+//     }
+// }
+
+
+
 using UnityEngine;
 
 [RequireComponent(typeof(SpriteRenderer))]
 public class PlayerVisualSetter : MonoBehaviour
 {
-    [SerializeField]
-    private SpriteRenderer spriteRenderer;
+    [SerializeField] private SpriteRenderer bodyRenderer;
+    [SerializeField] private SpriteRenderer leftShoeRenderer;
+    [SerializeField] private SpriteRenderer rightShoeRenderer;
 
     private void Start()
     {
-        Debug.Assert(spriteRenderer != null, $"Sprite Renderer is required for {nameof(PlayerVisualSetter)}");
+        Debug.Assert(bodyRenderer != null, $"Body SpriteRenderer is required for {nameof(PlayerVisualSetter)}");
+        Debug.Assert(leftShoeRenderer != null, $"Left Shoe SpriteRenderer is required for {nameof(PlayerVisualSetter)}");
+        Debug.Assert(rightShoeRenderer != null, $"Right Shoe SpriteRenderer is required for {nameof(PlayerVisualSetter)}");
     }
 
     public void SetVisual(PlayerVisual visual)
     {
         Debug.Assert(visual != null);
 
-        spriteRenderer.sprite = visual.Sprite;
-        spriteRenderer.color = visual.Color;
+        bodyRenderer.sprite = visual.Sprite;
+        bodyRenderer.color = visual.Color;
+
+        leftShoeRenderer.sprite = visual.LeftShoe;
+        rightShoeRenderer.sprite = visual.RightShoe;
     }
 }
